50328bb33b08807d22b3c2edbf3d1795b613e69c
ske-kr <54828276+ske-kr@users.noreply.github.com>
2022-08-23 12:48:35 +0900

    feat(login): add login modal skeleton

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,5 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import {useState} from "react";
+import ModalBase from "../components/ModalBase";
+import LoginModal from "../components/LoginModal";
 
 export default function Home() {
-  return <div>홈페이지</div>;
+  const [isActive, setIsActive] = useState(false);
+  const onClickCodeListOn = () => {
+    setIsActive(true);
+  };
+
+  const onClickModalOff = () => {
+    setIsActive(false);
+  };
+  return <div><div>홈페이지</div>
+    <button onClick={()=>setIsActive(true)}>
+      테스트 버튼
+    </button>
+    <ModalBase active={isActive} closeEvent={onClickModalOff}>
+      <LoginModal closeEvent={onClickModalOff} title="세부 항목">
+        <div className="overflow-y-scroll max-h-80">
+          요기에 로그인뭐시기 들어갈 예정
+        </div>
+      </LoginModal>
+    </ModalBase>
+</div>;
 }
