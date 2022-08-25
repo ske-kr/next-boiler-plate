e369c8acc39666a1d6b2897579d933b40be0a72b
ske-kr <54828276+ske-kr@users.noreply.github.com>
2022-08-25 10:55:51 +0900

    template added

--- next-app/src/components/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout(props: any) {
-  return (
-    <div className="pl-5 pr-5">
-      <div>{props.children}</div>
-    </div>
-  );
-}
--- next-app/src/services/storage.ts
+++
@@ -1,9 +0,0 @@
-import { supabaseClient } from "../utils/supabase";
-
-export async function getStorageImage(folderName: string, imgAddress: string) {
-  const { data, error } = await supabaseClient.storage
-    .from(folderName)
-    .download(imgAddress);
-  if (error) throw new Error(`Storage Error: ${error}`);
-  return data;
-}
